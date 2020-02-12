daa92efac83d321c5896c95df1b757ead27e27c2
Enzo Esperguin <enzoesper@hotmail.com>
2020-02-11 23:29:19 -0300

    adding some comments to explain the function that mimics the functionality of the require() function

--- module-system/index.js
+++ module-system/index.js
@@ -9,24 +9,26 @@ function loadModule(filename, module, require) {
 
 const require = (moduleName) => {
   console.log(`Require invoked for module ${moduleName}`);
+  // [1]
   const id = require.resolve(moduleName);
+  // [2]
   if (require.cache[id]) {
     return require.cache[id].exports;
   }
 
-  //module metadata
+  //[3] module metadata
   const module = {
     exports: {},
     id: id
   };
 
-  //update the cache
+  // [4] update the cache
   require.cache[id] = module;
 
-  //load the module
+  //[5] load the module
   loadModule(id, module, require);
 
-  //return exported variables
+  // [6] return exported variables
   return module.exports;
 };
 
@@ -34,3 +36,21 @@ require.cache = {};
 require.resolve = (moduleName) => {
   // resolve a full module id from the moduleName
 };
+
+
+// [1] A module name is accepted as input, and the very first thing that we do is
+// resolve the full path of the module, which we call id. This task is delegated to
+// require.resolve(), which implements a specific resolving algorithm
+// [2] If the module has already been loaded in the past, it should be available in the
+// cache. In this case, we just return it immediately
+// [3] If the module was not loaded yet, we set up the environment for the first load.
+// In particular, we create a module object that contains an exports property
+// initialized with an empty object literal. This property will be used by the code of
+// the module to export any public API. 
+// [4] The module object is cached. 
+// [5] The module source code is read from its file and the code is evaluated, as we
+// have seen before. We provide the module with the module object that we just
+// created, and a reference to the require() function. The module exports its
+// public API by manipulating or replacing the module.exports object. 
+// [6] Finally, the content of module.exports, which represents the public API of the
+// module, is returned to the caller.
