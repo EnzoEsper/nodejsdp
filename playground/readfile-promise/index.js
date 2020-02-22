00803d6f8fd8780d5fb759a829cbf5cfbbfc6156
Enzo Esperguin <enzoesper@hotmail.com>
2020-02-22 12:23:13 -0300

    readFile function to promise test

--- playground/readfile-promise/index.js
+++ playground/readfile-promise/index.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const resolve = require("path").resolve;
 
 function readFile(path) {
   return new Promise((resolve, reject) => {
@@ -9,6 +10,31 @@ function readFile(path) {
   });
 }
 
-readFile("./archivo1.txt")
+/* readFile("./archivo1.txt")
+  .then(data => console.log(data))
+  .catch(error => console.log(error)); */
+
+// aplicando promise chaining para leer el segundo archivo al que hace referencia el primero.
+// como el segundo then recibe el path al segundo archivo y readFile solo recibe un argumento
+// (el path) entonces se puede pasar directamente readFile y la promesa se encarga de ejecutarlo.
+/* readFile("./archivo1.txt")
+  .then(readFile)
+  .then(data => console.log(data))
+  .catch(error => console.log(error)); */
+
+// se leen los archivos 1 y 3 paralelamente para obtener las rutas a las que hacen referencia
+// para luego leer nuevamente de forma paralela el contenido de los archivos a los que hacian referencia 
+// las rutas.
+/* Promise.all([readFile('./archivo1.txt'), readFile('./archivo3.txt')])
+  .then(data => data.map(path => resolve(path)))
+  .then(data => Promise.all(data.map(readFile)))
+  .then(finalData => console.log(finalData))
+  .catch(error => console.error(error)); */
+
+// en este caso se van a obtener los datos de la promesa que se resuelva primero del arreglo
+Promise.race([readFile('./archivo1.txt'), readFile('./archivo3.txt')])
+  .then(resolve)
+  .then(readFile)
   .then(data => console.log(data))
   .catch(error => console.log(error));
+
