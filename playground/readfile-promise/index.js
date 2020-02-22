a4c20be675b5c7583a20fca74b339c368e791c76
Enzo Esperguin <enzoesper@hotmail.com>
2020-02-21 23:53:05 -0300

    adding a more general playground directory for tests purposes

---
+++ playground/readfile-promise/index.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+
+function readFile(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, "utf-8", (error, data) => {
+      if (error) return reject(error);
+      return resolve(data); 
+    });
+  });
+}
+
+readFile("./archivo1.txt")
+  .then(data => console.log(data))
+  .catch(error => console.log(error));
